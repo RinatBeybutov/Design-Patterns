5d303b796fcdafe17bdf3a8b88e5624a98c0f695
Rinat Beybutov <r.beybutov@kryptonite.ru>
2023-11-01 13:40:10 +0300

    added pattern singleton and command

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import Command.*;
 import Decorator.*;
 import Factory.FactoryMethod.ChicagoPizzaStore;
 import Factory.FactoryMethod.NYPizzaStore;
@@ -113,5 +114,58 @@ public class AppTest extends TestCase {
 
         pizza = chicagoStore.orderPizza("veggie");
         System.out.println("Joel ordered a " + pizza + "\n");
+    }
+
+    public void testClassicSingleton() {
+        Singleton.classic.Singleton singleton = Singleton.classic.Singleton.getInstance();
+        System.out.println(singleton.getDescription());
+    }
+
+    public void testEnumSingleton() {
+        Singleton.enumS.Singleton singleton = Singleton.enumS.Singleton.UNIQUE_INSTANCE;
+        System.out.println(singleton.getDescription());
+    }
+
+    public void testStaticInitializationSingleton() {
+        Singleton.staticInitialization.Singleton singleton = Singleton.staticInitialization.Singleton.getInstance();
+        System.out.println(singleton.getDescription());
+    }
+
+    public void testThreadSafeSingleton() {
+        Singleton.threadsafe.Singleton singleton = Singleton.threadsafe.Singleton.getInstance();
+        System.out.println(singleton.getDescription());
+    }
+
+    public void testCommand() {
+        RemoteControl remoteControl = new RemoteControl();
+
+        Light livingRoomLight = new Light("Living Room");
+        Light kitchenLight = new Light("Kitchen");
+        CeilingFan ceilingFan= new CeilingFan("Living Room");
+        GarageDoor garageDoor = new GarageDoor("Main house");
+        Stereo stereo = new Stereo("Living Room");
+
+        remoteControl.setCommand(0, livingRoomLight::on, livingRoomLight::off);
+        remoteControl.setCommand(1, kitchenLight::on, kitchenLight::off);
+        remoteControl.setCommand(2, ceilingFan::high, ceilingFan::off);
+
+        Command stereoOnWithCD = () -> {
+            stereo.on(); stereo.setCD(); stereo.setVolume(11);
+        };
+        remoteControl.setCommand(3, stereoOnWithCD, stereo::off);
+        remoteControl.setCommand(4, garageDoor::up, garageDoor::down);
+
+
+        remoteControl.onButtonWasPushed(0);
+        remoteControl.offButtonWasPushed(0);
+        remoteControl.onButtonWasPushed(1);
+        remoteControl.offButtonWasPushed(1);
+        remoteControl.onButtonWasPushed(2);
+        remoteControl.offButtonWasPushed(2);
+        remoteControl.onButtonWasPushed(3);
+        remoteControl.offButtonWasPushed(3);
+        remoteControl.onButtonWasPushed(4);
+        remoteControl.offButtonWasPushed(4);
+        remoteControl.onButtonWasPushed(5);
     }
 }
